a4a69499b1861719c749851b4959eb15dd2e3428
Bodie Shane <bodieshane68@gmail.com>
2016-01-06 08:17:58 -0700

    I added the 2 screens

---
+++ Modal Demo/RunningViewContoller.swift
@@ -0,0 +1,14 @@
+//
+//  RunningViewContoller.swift
+//  Modal Demo
+//
+//  Created by Shane, Bodie on 1/6/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class RunningViewController : ViewController
+{
+    
+}
---
+++ Modal Demo/favViewController.swift
@@ -0,0 +1,14 @@
+//
+//  favViewController.swift
+//  Modal Demo
+//
+//  Created by Shane, Bodie on 1/6/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import  UIKit
+
+class favViewController : ViewController
+{
+    
+}
